037e8bde5f3bf91ba40a1a8f201088b201c6611b
Andrew Chen <andrewkkchen@gmail.com>
2025-02-04 17:00:54 -0600

    Sped up image loading on articles and newsletters page

--- nextjs-frontend/src/app/articles/page.tsx
+++ nextjs-frontend/src/app/articles/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Head from 'next/head';
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { createClient } from "@supabase/supabase-js";
@@ -8,6 +9,48 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Layout } from "@/components/Layout";
 import ReactMarkdown from 'react-markdown';
+
+const LANDSCAPE_IMAGES = [
+  "/landscape_backgrounds/abbie-parks-XA1-J2rRGVw-unsplash.jpg",
+  "/landscape_backgrounds/casper-van-battum-icWhBzRNUFw-unsplash.jpg",
+  "/landscape_backgrounds/christopher-ott-qdcRECrSppU-unsplash.jpg",
+  "/landscape_backgrounds/david-clode-8tZJG2t30fI-unsplash.jpg",
+  "/landscape_backgrounds/david-clode-iQi_QFMfBZI-unsplash.jpg",
+  "/landscape_backgrounds/david-clode-NugxTvRaQT0-unsplash.jpg",
+  "/landscape_backgrounds/emanuel-rodriguez-2hdbY4Xaihw-unsplash.jpg",
+  "/landscape_backgrounds/harshal-s-hirve-oZ0xzQFVCWY-unsplash.jpg",
+  "/landscape_backgrounds/henry-schneider-s3sJ5j2ml2o-unsplash.jpg",
+  "/landscape_backgrounds/james-wainscoat-WJ2ev0gQo4k-unsplash.jpg",
+  "/landscape_backgrounds/jason-mitrione-MLAXy8PcGNk-unsplash.jpg",
+  "/landscape_backgrounds/matt-seymour-qLvikxzTtWY-unsplash.jpg",
+  "/landscape_backgrounds/nyusha-svoboda-_AaHmPTYzig-unsplash.jpg",
+  "/landscape_backgrounds/patrick-hendry-_gHLz18DEpE-unsplash.jpg",
+  "/landscape_backgrounds/patrick-hendry-hqcxvmNyFyg-unsplash.jpg",
+  "/landscape_backgrounds/patrick-hendry-vOFzgDqPh3Y-unsplash (1).jpg",
+  "/landscape_backgrounds/peter-neumann-Uofb3of6CCQ-unsplash.jpg",
+  "/landscape_backgrounds/phoenix-han-2v_bZYAlKQ4-unsplash.jpg",
+  "/landscape_backgrounds/sandra-alekseeva-h4vXoyKX_-Y-unsplash.jpg",
+  "/landscape_backgrounds/timothy-dykes-3EUmV_AwKoA-unsplash.jpg",
+  "/landscape_backgrounds/timothy-dykes-3JrIpTJ7tkM-unsplash.jpg",
+  "/landscape_backgrounds/viktor-talashuk-0_cIDZw6rgc-unsplash.jpg",
+  "/landscape_backgrounds/wolfgang-hasselmann-PUcrsXh9V4s-unsplash.jpg",
+  "/landscape_backgrounds/zdenek-machacek-DwrVhMZmcaY-unsplash.jpg",
+] as const;
+
+const ImagePreloader = () => {
+  return (
+    <>
+      {LANDSCAPE_IMAGES.map((src, index) => (
+        <link 
+          key={src}
+          rel="preload"
+          as="image"
+          href={src}
+        />
+      ))}
+    </>
+  );
+};
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -19,34 +62,8 @@ export default function Articles() {
   const [articles, setArticles] = useState<any[]>([]);
 
   const getRandomLandscapeImage = () => {
-    const images = [
-      "/landscape_backgrounds/abbie-parks-XA1-J2rRGVw-unsplash.jpg",
-      "/landscape_backgrounds/casper-van-battum-icWhBzRNUFw-unsplash.jpg",
-      "/landscape_backgrounds/christopher-ott-qdcRECrSppU-unsplash.jpg",
-      "/landscape_backgrounds/david-clode-8tZJG2t30fI-unsplash.jpg",
-      "/landscape_backgrounds/david-clode-iQi_QFMfBZI-unsplash.jpg",
-      "/landscape_backgrounds/david-clode-NugxTvRaQT0-unsplash.jpg",
-      "/landscape_backgrounds/emanuel-rodriguez-2hdbY4Xaihw-unsplash.jpg",
-      "/landscape_backgrounds/harshal-s-hirve-oZ0xzQFVCWY-unsplash.jpg",
-      "/landscape_backgrounds/henry-schneider-s3sJ5j2ml2o-unsplash.jpg",
-      "/landscape_backgrounds/james-wainscoat-WJ2ev0gQo4k-unsplash.jpg",
-      "/landscape_backgrounds/jason-mitrione-MLAXy8PcGNk-unsplash.jpg",
-      "/landscape_backgrounds/matt-seymour-qLvikxzTtWY-unsplash.jpg",
-      "/landscape_backgrounds/nyusha-svoboda-_AaHmPTYzig-unsplash.jpg",
-      "/landscape_backgrounds/patrick-hendry-_gHLz18DEpE-unsplash.jpg",
-      "/landscape_backgrounds/patrick-hendry-hqcxvmNyFyg-unsplash.jpg",
-      "/landscape_backgrounds/patrick-hendry-vOFzgDqPh3Y-unsplash (1).jpg",
-      "/landscape_backgrounds/peter-neumann-Uofb3of6CCQ-unsplash.jpg",
-      "/landscape_backgrounds/phoenix-han-2v_bZYAlKQ4-unsplash.jpg",
-      "/landscape_backgrounds/sandra-alekseeva-h4vXoyKX_-Y-unsplash.jpg",
-      "/landscape_backgrounds/timothy-dykes-3EUmV_AwKoA-unsplash.jpg",
-      "/landscape_backgrounds/timothy-dykes-3JrIpTJ7tkM-unsplash.jpg",
-      "/landscape_backgrounds/viktor-talashuk-0_cIDZw6rgc-unsplash.jpg",
-      "/landscape_backgrounds/wolfgang-hasselmann-PUcrsXh9V4s-unsplash.jpg",
-      "/landscape_backgrounds/zdenek-machacek-DwrVhMZmcaY-unsplash.jpg",
-    ];
-    const randomIndex = Math.floor(Math.random() * images.length);
-    return images[randomIndex];
+    const randomIndex = Math.floor(Math.random() * LANDSCAPE_IMAGES.length);
+    return LANDSCAPE_IMAGES[randomIndex];
   };
 
   const fetchArticles = async () => {
@@ -68,6 +85,9 @@ export default function Articles() {
 
   return (
     <Layout>
+      <Head>
+        <ImagePreloader />
+      </Head>
       <div className="fixed inset-0 -z-10">
         <div
           className="absolute inset-0 transition-opacity duration-1000 ease-in-out"
