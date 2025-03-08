70e90d7fda41881277f9c2cfbd7405e181a67a79
Andrew Chen <andrewkkchen@gmail.com>
2025-03-08 17:23:41 -0600

    Cleaned up test functions

--- backend/script.py
+++ backend/script.py
@@ -1,12 +1,12 @@
 from utils.aws_utils import upload_to_s3
 from utils.other_utils import (
     article_selection,
+    article_selection_JUFO,
     generate_gpt_paper_summary,
-    fetch_venue_info,
 )
-from utils.email_utils import render_template, smtp_send_email, resend_send_email
-from utils.supabase_utils import supabase_newsletters_POST, supabase_articles_GET
-from utils.ss_api import fetch_bulk_articles, fetch_paper_details
+from utils.email_utils import render_template, resend_send_email
+from utils.supabase_utils import supabase_newsletters_POST
+from utils.ss_api import fetch_bulk_articles
 import markdown
 import json
 
@@ -26,69 +26,31 @@ def test_summaries():
         f.write(writestring)
 
 
-def tmp_test():
-    """TEMPORARY test for article fetching"""
-    articles = fetch_bulk_articles()
-    with open("test_articles.json", "w") as f:
-        json.dump(articles, f)
-
-
 def script(event, context):
     """Main script to run the newsletter generation and email sending process."""
     from datetime import datetime
 
     data = fetch_bulk_articles()
-    _, result = article_selection(data)
-    for article in result:
-        article["authors"] = [author["name"] for author in article["authors"]]
-        article["authors"] = ", ".join(article["authors"])
-        article["llm_summary"] = markdown.markdown(article["llm_summary"])
+    result = article_selection_JUFO(data)
+    if len(result) > 0:
+        for article in result:
+            article["authors"] = [author["name"] for author in article["authors"]]
+            article["authors"] = ", ".join(article["authors"])
+            article["llm_summary"] = markdown.markdown(article["llm_summary"])
 
-    email_html_template = render_template(result)
-    resend_send_email(email_html_template)
-    upload_to_s3(email_html_template)
-    data = {
-        "name": f"{datetime.now().strftime('%m-%d-%Y')}.html",
-        "link": f"https://myconews.s3-us-west-1.amazonaws.com/{datetime.now().strftime('%m-%d-%Y')}.html",
-    }
-    try:
+        email_html_template = render_template(result)
+        resend_send_email(email_html_template)
+        upload_to_s3(email_html_template)
+        data = {
+            "name": f"{datetime.now().strftime('%m-%d-%Y')}.html",
+            "link": f"https://myconews.s3-us-west-1.amazonaws.com/{datetime.now().strftime('%m-%d-%Y')}.html",
+        }
         supabase_newsletters_POST(data)
-    except Exception as e:
-        return
-
-
-def tmp_cleaning_JUFO():
-    """TEMPORARY function for cleaning JUFO data"""
-    import pandas as pd
-
-    df = None
-    with open("jfp-export.csv", "r") as f:
-        df = pd.read_csv(f)
-
-    print(len(df))
-    df = df.drop_duplicates(subset=["Name"])
-    print(len(df))
-
-    with open("jfp-export-cleaned.csv", "w") as f:
-        df.to_csv(f, index=False)
-
-
-def tmp_test_paper_detail():
-    """TEMPORARY function for testing paper details"""
-    # paper_id = "51121bfb8d93362607e71e795aa3e71945156b4f"
-    paper_id = "0309d98e85ffd497806f86af62c2bd9ed74a38de"
-    data = fetch_paper_details(paper_id)
-    print(json.dumps(data, indent=2))
+    else:
+        print(
+            f"No articles selected for the newsletter for the week of {datetime.now().strftime('%m-%d-%Y')}."
+        )
 
 
 if __name__ == "__main__":
-    from utils.other_utils import article_selection_JUFO
-
-    # script("event", "context")
-    # tmp_test()
-    # articles = article_selection_JUFO([])
-    test_summaries()
-    # tmp_cleaning_JUFO()
-    # tmp_test_paper_detail()
-    # articles = article_selection_JUFO(fetch_bulk_articles())
-    # print(articles)
+    script("event", "context")
